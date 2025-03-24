f19f2f109e6001b40e154f1df2fbbe5dc8d94b40
Михаил Кузин <mk@MacBook-Air-Mihail.local>
2025-03-24 13:32:27 +0300

    add Pete the baker

--- pete_the_baker.py
+++ pete_the_baker.py
@@ -1,10 +1,16 @@
 def cakes(recipe, available):
     number_cakes = 0
-    for key in recipe.keys():
-        if key in available.keys():
-            print(True)
+    number_available = []
+    for key_r in recipe.keys():
+        if key_r in available.keys():
+            print(key_r)
+            number_available.append(available.get(key_r) // recipe.get(key_r))
         else:
-            print(False)
+            return number_cakes
+    number_cakes = min(number_available)
     return number_cakes
 
-cake = cakes({"flour": 500, "sugar": 200, "eggs": 1}, {"flour": 500, "sugar": 200, "salt": 1})
+cake = cakes({'flour': 200, 'sugar': 200},
+             {'flour': 1200, 'eggs': 5, 'milk': 200})
+
+print(cake)
